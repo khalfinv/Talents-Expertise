bb6c9ce649daf6b32434b87a5c44a2b6103664f2
Vicki Khalfin <khalfinv@gmail.com>
2017-07-19 23:43:28 +0300

    link to DB

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-  database: 'mongodb://localhost:27017/talent_expertise',
+  database: 'mongodb://khalfinv:99311vicka@ds161032.mlab.com:61032/talents_expertise',
   secret: 'yoursecret'
 }
